55134c6da5a36841bb8bea3a69ac7428605bbe1c
joaquin1990 <joaquingarese@gmail.com>
2023-05-23 19:10:15 -0300

    vercel check

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,6 @@ import farms from './documents/farms'
 import houses from './documents/houses'
 import countries from './documents/countries'
 import imageWithAlt from './objects/imageWithAlt'
-import cattles from './documents/cattles'
+// import cattles from './documents/cattles'
 
-export const schemaTypes = [farms, houses, countries, imageWithAlt, cattles]
+export const schemaTypes = [farms, houses, countries, imageWithAlt]
