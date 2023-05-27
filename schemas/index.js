c2b1c7b9be2915067130072c9166ff42bbda50e0
joaquin1990 <joaquingarese@gmail.com>
2023-05-27 12:37:39 -0300

    state schema fixing

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,7 @@
 import farms from './documents/farms'
 import houses from './documents/houses'
 import countries from './documents/countries'
+import states from './documents/states'
 import imageWithAlt from './objects/imageWithAlt'
 
-export const schemaTypes = [farms, houses, countries, imageWithAlt]
+export const schemaTypes = [farms, houses, countries, states, imageWithAlt]
