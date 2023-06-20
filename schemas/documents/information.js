f858ad1950b1f115ad1d39747c3a8a2853640b6e
joaquin1990 <joaquingarese@gmail.com>
2023-06-20 11:33:09 -0300

    information schema

---
+++ schemas/documents/information.js
@@ -0,0 +1,64 @@
+export default {
+  name: 'information',
+  type: 'document',
+  title: 'Informacion',
+  fields: [
+    {
+      name: 'novillos',
+      type: 'number',
+      title: 'Precio (USD) Novillos',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'cows',
+      type: 'number',
+      title: 'Precio (USD) Vacas',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'vaquillonas',
+      type: 'number',
+      title: 'Precio (USD) Vaquillonas',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'corderoPesado',
+      type: 'number',
+      title: 'Precio (USD) Cordero Pesado',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'capones',
+      type: 'number',
+      title: 'Precio (USD) Capones',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'ovejas',
+      type: 'number',
+      title: 'Precio (USD) Ovejas',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'vacunos',
+      type: 'number',
+      title: 'Mercado de Faenas Vacunos',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'ovejas',
+      type: 'number',
+      title: 'Mercado de Faenas Ovinos',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'customDate',
+      type: 'date',
+      title: 'Fecha informacion (solamente el último dia de la semana que marca ACG)',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
--- schemas/index.js
+++ schemas/index.js
@@ -4,5 +4,6 @@ import countries from './documents/countries'
 import states from './documents/states'
 import cattle from './documents/cattle'
 import imageWithAlt from './objects/imageWithAlt'
+import information from './documents/information'
 
-export const schemaTypes = [farms, houses, countries, states, cattle, imageWithAlt]
+export const schemaTypes = [farms, houses, countries, states, cattle, imageWithAlt, information]
